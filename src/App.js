0212ebffa392799436e933353476952f67221025
rotembcohen <rotembcohen@gmail.com>
2018-09-20 16:05:08 -0400

    manage added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import List from 'antd/lib/list';
 import Input from 'antd/lib/input';
-import { Row, Col } from 'antd/lib/grid';
 import DatePicker from 'antd/lib/date-picker';
 import TimePicker from 'antd/lib/time-picker';
 import moment from 'moment';
 import Button from 'antd/lib/button';
 import Form from 'antd/lib/form';
 import InputNumber from 'antd/lib/input-number';
+import Layout  from 'antd/lib/layout';
+import { Row, Col } from 'antd/lib/grid';
+const { Header, Footer, Content } = Layout;
 
 class App extends Component {
     constructor() {
@@ -21,18 +21,29 @@ class App extends Component {
         let secondDate = moment(firstDate).add(2, 'days');
 
         this.state = {
-            value: null,
-            data: null,
             slotAmount: 12,
             slotDuration: 20,
             location: 'Conference Room 3A',
             firstDate: firstDate,
             firstTime: moment().hour(11).minutes(0).seconds(0),
             secondDate: secondDate,
-            secondTime: moment().hour(12).minutes(0).seconds(0)
+            secondTime: moment().hour(12).minutes(0).seconds(0),
+            currentLottery: null
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.executeLottery = this.executeLottery.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadCurrentLottery();
+    }
+
+    loadCurrentLottery() {
+        fetch('/lottery/18/')
+            .then(response=>response.json())
+            .then(data => this.setState({currentLottery:data}))
+            .catch(error => console.error('Errors', error))
     }
 
     getWeekday(dayINeed) {
@@ -54,7 +65,7 @@ class App extends Component {
             this.stringifyDate(this.state.firstDate,this.state.firstTime),
             this.stringifyDate(this.state.secondDate,this.state.secondTime)
         ];
-        fetch('http://localhost:8000/createBatch/',
+        fetch('/createBatch/',
             {
                 method: 'POST',
                 headers: {
@@ -67,10 +78,29 @@ class App extends Component {
                     slotDuration: this.state.slotDuration,
                     location: this.state.location
                 })
-            }
-            )
+            })
             .then(response => {
-                console.log(response.status)
+                console.log(response.status);
+                this.loadCurrentLottery();
+            })
+            .catch(error => console.error('Error:', error))
+    }
+
+    executeLottery() {
+        fetch('/lottery/18/',
+            {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    isFinished: true
+                })
+            })
+            .then(response => {
+                console.log(response.status);
+                this.loadCurrentLottery();
             })
             .catch(error => console.error('Error:', error))
     }
@@ -81,7 +111,7 @@ class App extends Component {
 
     renderItem(item) {
         let itemTime = (new Date(item.startTime)).toLocaleString('en-GB', {timeZone: 'UTC'});
-        return (<List.Item>{itemTime} {item.winner}</List.Item>);
+        return (<List.Item>{itemTime} {item.winner?item.winner:"Open"}</List.Item>);
     }
 
     render() {
@@ -95,65 +125,79 @@ class App extends Component {
             sm: { span: 16 },
           },
         };
-        let items = this.state.data ? this.state.data.slots : [];
+
+        let items = this.state.currentLottery ? this.state.currentLottery.slots : [];
+        let currentFinished = this.state.currentLottery ? this.state.currentLottery.isFinished : true;
+        let renderExecuteButton = currentFinished ? (<Button onClick={this.executeLottery} type='danger' disabled>Execute</Button>)
+                : (<Button onClick={this.executeLottery} type='danger'>Execute</Button>);
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <div className="Dashboard">
-                    <Row justify="center">
-                        <Col span={10} style={{margin: 10}}>
-                            <h1>Create</h1>
-                            <Form align="space-between">
-                                <Form.Item {...formItemLayout} label="Slots Per Day:">
-                                    <InputNumber
-                                        value={this.state.slotAmount}
-                                        onChange={e=>{this.setState({slotAmount:e.target.value})}}
-                                    />
-                                </Form.Item>
-                                <Form.Item {...formItemLayout} label="Time Per Slot:">
-                                    <InputNumber
-                                        value={this.state.slotDuration}
-                                        onChange={e=>{this.setState({slotDuration:e.target.value})}}
-                                    />
-                                </Form.Item>
-                                <Form.Item {...formItemLayout} label="Location:">
-                                    <Input
-                                        value={this.state.location}
-                                        onChange={e=>{this.setState({location:e.target.value})}}
-                                    />
-                                </Form.Item>
-                                <Form.Item {...formItemLayout} label="First Date:">
-                                    <DatePicker
-                                        value={this.state.firstDate}
-                                        onChange={d=>{this.setState({firstDate:d})}}
-                                    />
-                                    <TimePicker
-                                        value={this.state.firstTime}
-                                        onChange={d=>{this.setState({firstTime:d})}}
-                                        format={"HH:mm"}
-                                    />
-                                </Form.Item>
-                                <Form.Item {...formItemLayout} label="Second Date:">
-                                    <DatePicker
-                                        value={this.state.secondDate}
-                                        onChange={d=>{this.setState({secondDate:d})}}
-                                    />
-                                    <TimePicker
-                                        value={this.state.secondTime}
-                                        onChange={d=>{this.setState({secondTime:d})}}
-                                        format={"HH:mm"}
-                                    />
-                                </Form.Item>
-                                <Button onClick={this.handleSubmit} type="primary">Create</Button>
-                            </Form>
-                        </Col>
-                    </Row>
-                </div>
-
+                <Layout>
+                    <Header className="App-header">
+                        <h1 className="App-title">Massage Lottery Admin</h1>
+                    </Header>
+                    <Content className="Dashboard">
+                        <Row>
+                            <Col span={12}>
+                                <h1>Create</h1>
+                                <Form id="CreateForm">
+                                    <Form.Item {...formItemLayout} label="Slots Per Day:">
+                                        <InputNumber
+                                            value={this.state.slotAmount}
+                                            onChange={e=>{this.setState({slotAmount:e.target.value})}}
+                                        />
+                                    </Form.Item>
+                                    <Form.Item {...formItemLayout} label="Time Per Slot:">
+                                        <InputNumber
+                                            value={this.state.slotDuration}
+                                            onChange={e=>{this.setState({slotDuration:e.target.value})}}
+                                        />
+                                    </Form.Item>
+                                    <Form.Item {...formItemLayout} label="Location:">
+                                        <Input
+                                            className="textInput"
+                                            value={this.state.location}
+                                            onChange={e=>{this.setState({location:e.target.value})}}
+                                        />
+                                    </Form.Item>
+                                    <Form.Item {...formItemLayout} label="First Date:">
+                                        <DatePicker
+                                            value={this.state.firstDate}
+                                            onChange={d=>{this.setState({firstDate:d})}}
+                                        />
+                                        <TimePicker
+                                            value={this.state.firstTime}
+                                            onChange={d=>{this.setState({firstTime:d})}}
+                                            format={"HH:mm"}
+                                        />
+                                    </Form.Item>
+                                    <Form.Item {...formItemLayout} label="Second Date:">
+                                        <DatePicker
+                                            value={this.state.secondDate}
+                                            onChange={d=>{this.setState({secondDate:d})}}
+                                        />
+                                        <TimePicker
+                                            value={this.state.secondTime}
+                                            onChange={d=>{this.setState({secondTime:d})}}
+                                            format={"HH:mm"}
+                                        />
+                                    </Form.Item>
+                                    <Button onClick={this.handleSubmit} type="primary">Create</Button>
+                                </Form>
+                            </Col>
+                            <Col span={12}>
+                                <h1>Manage</h1>
+                                {renderExecuteButton}
+                                <List
+                                    dataSource={items}
+                                    renderItem={this.renderItem}
+                                />
+                            </Col>
+                        </Row>
+                    </Content>
+                    <Footer/>
+                </Layout>
             </div>
         );
     }
@@ -161,23 +205,3 @@ class App extends Component {
 }
 
 export default App;
-
-/*
-<Col span={10}>
-    <form onSubmit={this.handleSubmit}>
-        Get slot count for
-        <Input
-            size="default"
-            id="slotIdInput"
-            onChange={this.handleChange}
-        />
-        <Button type="primary" onClick={()=>this.handleSubmit()}>Get List</Button>
-    </form>
-    <div className="List">
-        <List
-            dataSource={items}
-            renderItem={this.renderItem}
-        />
-    </div>
-</Col>
- */
